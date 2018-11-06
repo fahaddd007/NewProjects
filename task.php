574b7e9cae40a4542bacaa7e88a248e556e6e530
fahaddd007 <44797101+fahaddd007@users.noreply.github.com>
2018-11-06 12:08:05 +0500

    Edit File

---
+++ task.php
@@ -0,0 +1,22 @@
+<?php
+
+$q = "j";
+
+if($q == "a" || $q == "A"  || $q == "e" ||$q == "E" || $q == "i" ||$q == "I"|| $q == "o"|| $q == "O"|| $q == "u" ||$q == "U")
+{
+	
+	echo "<h1>Correct ans</h1>";
+	
+}
+else{
+	
+	echo "<h1>wrong ans</h1>";
+	
+}
+
+
+
+
+
+?>
+
