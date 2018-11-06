f2142c5b343834243c36158f4aa34f6663d8b313
fahaddd007 <44797101+fahaddd007@users.noreply.github.com>
2018-11-06 12:02:27 +0500

    Add files via upload

---
+++ second.php
@@ -0,0 +1,15 @@
+<?php
+
+$a=10;
+$b=2;
+echo"<h2>";
+echo"Subtraction:<br>".($a-$b);
+echo"<br><br>";
+echo"Addition:<br>".($a+$b);
+echo"<br><br>";
+echo"Multiplication:<br>".($a*$b);
+echo"<br><br>";
+echo"Division:<br>".($a/$b);
+echo"</h2>";
+
+?>
